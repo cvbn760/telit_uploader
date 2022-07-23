25687be19e3448df66fa023517ea13a7a1d7a9aa
cvbn760 <ross31456@gmail.com>
2022-07-23 13:59:37 +0300

    Загрузка файлов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ public class Main {
     private static SerialPort serialPort;
     private static boolean currentTask = false;
     private static InputStream is = null;
-    private static OutputStream os = null;
 
     private static boolean openConnection(String port){
         serialPort = new SerialPort("COM" + port);
@@ -37,7 +36,6 @@ public class Main {
         System.out.println(cmd);
         String data = "";
 
-        up:
         while (true){ // Ждем ответ
             Thread.sleep(50);
             data = serialPort.readString();
@@ -51,44 +49,83 @@ public class Main {
                 System.out.println(data + "\r\n");
                 return false;
             }
-            data = "";
         }
     }
 
-    private static void sendFile(String source, String destenation) throws IOException, SerialPortException, InterruptedException {
-        serialPort.writeString(destenation + "\r");
-        System.out.println(destenation + "\r");
-        Thread.sleep(500);
-        String data = serialPort.readString();
-        System.out.println(data);
-        if(data.contains(">>>")){
-//            System.out.println(">>>");
-        }else {
-//            System.out.println("ERROR");
-            return;
-        }
+    private boolean deleteFile(String fileName, String destenation){
+        // Составляем команду для удаления файла
+        StringBuffer sb = new StringBuffer();
 
+        return true;
+    }
+
+    private static boolean sendFile(String fileName, String source, String destenation) throws IOException, SerialPortException, InterruptedException {
+        // Находим файл по указанному пути и определяем размер
+        File file = new File(source + fileName);
+        long size = file.length();
+        System.out.println(source + fileName + "\r\nsize: " + size + " bytes");
+
+        StringBuffer sb = new StringBuffer();
+        sb.delete(0, sb.length());
+
+        // Переходим в указанную дирректорию в модеме
+        sb.append("AT#M2MCHDIR=");
+        sb.append("\"");
+        sb.append(destenation);
+        sb.append("\"");
+        sb.append("\r");
+        if(!sendAtCmd(sb.toString()));
+
+        // Составляем команду для отправки
+        sb.delete(0, sb.length());
+        sb.append("AT#M2MWRITE=");
+        sb.append("\"");
+        sb.append(fileName);
+        sb.append("\",");
+        sb.append(size);
+        sb.append("\r");
+        if(!sendAtCmd(sb.toString())) return false;
+
+        sb.delete(0, sb.length());
+        // Передаем файл
         try {
-            is = new FileInputStream(source);
+            is = new FileInputStream(file);
             byte[] buffer = new byte[1024];
             int length;
             while ((length = is.read(buffer)) > 0) {
                 serialPort.writeBytes(buffer);
             }
-            Thread.sleep(500);
-            data = serialPort.readString();
-            System.out.println(data);
-        } finally {
+            // После загрузки файла ожидаем ответ
+            String data = "";
+            while (true){ // Ждем ответ
+                Thread.sleep(50);
+                data = serialPort.readString();
+                if(data == null) data = "";
+                data = data.trim();
+                if (data.contains("OK")) {
+                    System.out.println(data + "\r\n");
+                    is.close();
+                    return true;
+                }
+                if (data.contains("ERROR")) {
+                    System.out.println(data + "\r\n");
+                    is.close();
+                    return false;
+                }
+            }
+        }catch (Exception exc){
             is.close();
+            exc.printStackTrace();
+            return false;
         }
     }
 
     public static void main(String[] args) throws SerialPortException, InterruptedException, IOException {
 
 //        String port = args[0];
-//        String packAddr = args[1];
+//        String sourceAddr = args[1];
         String port = "29";
-        String packAddr = "C:/8450110539_DSP";
+        String sourceAddr = "C:/8450110539_DSP/";
 
         // Показать доступные COM порты
         System.out.println("Ports available: ");
@@ -107,8 +144,8 @@ public class Main {
             System.out.println(">>>>>>>>>>>>>> COM" + port + " was opened\r\n");
         }
 
-        // Загрузка АТ команд
-        if(!sendAtCmd("AT$GPSP=0\r")) return;
+//        // Загрузка АТ команд
+        if(!sendAtCmd("AT$GPSP=1\r"));
         if(!sendAtCmd("AT$GPSSAV\r")) return;
         if(!sendAtCmd("AT#V24CFG=3,1,1\r")) return;
         if(!sendAtCmd("AT#V24=3,1\r")) return;
@@ -125,14 +162,14 @@ public class Main {
         System.out.println("All cmds upload");
 
 //         Загрузка файлов
-//        sendFile("C:/8450110539_DSP/era.bin", "\r\nAT#M2MWRITE=\"/data/azc/mod/era.bin\",572458");
-//        Thread.sleep(500);
-//        sendFile("C:/8450110539_DSP/factory.cfg", "\r\nAT#M2MWRITE=\"/data/azc/mod/factory.cfg\",46");
-//        Thread.sleep(500);
-//        sendFile("C:/8450110539_DSP/softdog.bin", "\r\nAT#M2MWRITE=\"/data/azc/mod/softdog.bin\",13096");
-//        Thread.sleep(500);
-//        sendFile("C:/8450110539_DSP/adb_credentials", "\r\nAT#M2MWRITE=\"/var/run/adb_credentials\",18");
-//        Thread.sleep(500);
+        if (!sendFile("era.bin", sourceAddr, "/data/azc/mod/")) return;
+        if (!sendFile("factory.cfg", sourceAddr, "/data/azc/mod/")) return;
+        if (!sendFile("softdog.bin", sourceAddr, "/data/azc/mod/")) return;
+        if (!sendFile("adb_credentials", sourceAddr, "/var/run/")) return;
 
+
+        serialPort.closePort();
+        System.out.println("Port: COM" + port + " was closed");
+        System.out.println("Firmware completed successfully");
     }
 }
